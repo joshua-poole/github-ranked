f71bd1eb227ba30805fcd749ef8072e018de8110
NathanTheDev <nathan.smith1922@gmail.com>
2026-03-14 23:34:02 +1100

    feat: saved user to db when first searching them

--- src/server/services/dashboard/searchUser.ts
+++ src/server/services/dashboard/searchUser.ts
@@ -1,30 +1,49 @@
+import { prisma } from '#/db'
 import { TRPCError } from '@trpc/server'
 
-export async function searchUser(username: String): Promise<{ login: string }> {
-  const res = await fetch(`https://api.github.com/users/${username}`, {
-    headers: {
-      Accept: 'application/vnd.github+json',
-      'User-Agent': 'github-ranked-app',
-      ...(process.env.GITHUB_TOKEN && {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      }),
-    },
+export async function searchUser(username: string): Promise<{ login: string }> {
+  const dbUser = await prisma.user.findUnique({
+    where: { username: username },
   })
 
-  if (res.status === 404) {
-    throw new TRPCError({
-      code: 'NOT_FOUND',
-      message: `GitHub user "${username}" not found`,
+  if (!dbUser) {
+    const res = await fetch(`https://api.github.com/users/${username}`, {
+      headers: {
+        Accept: 'application/vnd.github+json',
+        'User-Agent': 'github-ranked-app',
+        ...(process.env.GITHUB_TOKEN && {
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        }),
+      },
     })
+
+    if (res.status === 404) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: `GitHub user "${username}" not found`,
+      })
+    }
+
+    if (!res.ok) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'GitHub API error',
+      })
+    }
+
+    const user = await res.json()
+
+    await prisma.user.create({
+      data: {
+        username: user.login,
+        rank: 'UNRANKED',
+        elo: 0,
+        placementCompleted: false,
+      },
+    })
+
+    return { login: user.login as string }
   }
 
-  if (!res.ok) {
-    throw new TRPCError({
-      code: 'INTERNAL_SERVER_ERROR',
-      message: 'GitHub API error',
-    })
-  }
-
-  const user = await res.json()
-  return { login: user.login as string }
+  return { login: dbUser.username }
 }
